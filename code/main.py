5bca27e028bd2a389edc0c97ebf65db3cff89a68
FinnMcKinley <finnmckinley@proton.me>
2023-06-16 00:28:49 +0000

    Imported Flask in Replit for Testing

--- code/main.py
+++ code/main.py
@@ -1,1 +1,10 @@
-print("Does this work?")
+from flask import Flask, render_template    # Imports Flask Library
+
+app = Flask(__name__) 
+
+@app.route('/')    # Route to Homepage
+def root():
+  return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
